b837081798cf87a5938a51323727f482ff051efb
Nikita <nike04@mail.ru>
2021-05-18 19:52:45 +0500

    start bot

---
+++ TelegramWaifu_bot.py
@@ -0,0 +1,22 @@
+import telebot
+import config
+
+bot = telebot.TeleBot(config.TOKEN)
+
+# приветствие
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    sti = open('static/welcome.webp', 'rb')
+    bot.send_sticker(message.chat.id, sti)
+
+    bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный украсить твое одиночество    .".format(message.from_user, bot.get_me()), parse_mode='html')
+
+# ответ на сообщение "привет"
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.from_user.id, 'Привет!')
+    else:
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+
+bot.polling(none_stop=True)
---
+++ config.py
@@ -0,0 +1,1 @@
+TOKEN = '1678506351:AAGigvgqq_NHxQbAi-jI7KuQVC_TOJRXPcg' # bot token from @BotFather
